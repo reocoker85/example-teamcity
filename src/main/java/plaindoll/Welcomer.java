8614c3fbe5bb3eb29a84991bcad3093af1d6b657
reocoker85 <139924408+reocoker85@users.noreply.github.com>
2024-07-11 00:38:01 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,4 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
+	public String sayKiSh(){
+                return " Nobody knew that at this moment hunter Sebastian";
+	}
 }
